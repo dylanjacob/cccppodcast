15301294c608dbf5f25d05ecf151a0154d1c3abd
Dylan Jacob <dylan.jacob@bubblecore.net>
2019-02-02 11:44:50 -0800

    Added more client stuff and dockerized it

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -10,12 +10,15 @@ class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
