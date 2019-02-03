2a6e8eb2f96c09f2464eef1f546c6d79b5e45493
Dylan Jacob <dylan.jacob@bubblecore.net>
2019-02-02 22:51:59 -0800

    Added auth routes for client

--- services/client/src/components/NavBar.jsx
+++ services/client/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => (
   // eslint-disable-next-line
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a className="navbar-brand" href="#">
+    <Link to="#" className="navbar-brand">
       <strong className="navbar-item">{props.title}</strong>
-    </a>
+    </Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
