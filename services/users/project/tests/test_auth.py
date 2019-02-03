72c944875d3aad64d10114a13f4bf6cbfde94e31
Dylan Jacob <dylan.jacob@bubblecore.net>
2019-02-02 21:33:40 -0800

    Commented out unused libraries in Auth test, but I think I'll need them later?

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -6,8 +6,8 @@ import unittest
 
 from flask import current_app
 
-from project import db
-from project.api.models import User
+# from project import db
+# from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
